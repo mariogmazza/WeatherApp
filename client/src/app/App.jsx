0452e9ae031df6d889142d64e77ce8874c973565
mariogmazza <mariomazza01@gmail.com>
2018-12-04 21:00:40 -0500

    working on deployment

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import WeatherApp from '../containers/WeatherApp'
 import { Route, Switch, withRouter } from 'react-router-dom'
 
