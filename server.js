f2297a491677fb372de889b1d7c716d9eadc8911
mariogmazza <mariomazza01@gmail.com>
2018-12-04 21:02:19 -0500

    more deployment work

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use('/api/getweather', routes.getWeatherRoute);
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, './client/build')));
 
   // Handle React routing, return all requests to React app
   app.get('*', (req, res) => {
